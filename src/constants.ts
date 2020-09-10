8ecce088df929130911a6f70f7c63fa89542db01
Emily Kasbohm <emily_kasbohm@brown.edu>
2020-09-10 11:30:42 -0400

    Use proxy URLs

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const BASE_URLS = {
-  qa: "***REMOVED***",
-  prod: "***REMOVED***"
+  qa: "https://webservices-proxy.cis-qas.brown.edu/",
+  prod: "https://webservices-proxy.brown.edu/"
 };
